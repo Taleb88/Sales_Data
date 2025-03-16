f29851deec522a2aae13d90afedd62efa7fa3f55
Taleb88 <tdamaree18@gmail.com>
2025-03-16 19:46:18 -0400

    attempting to create new dataframes based on order priorities on us holidays; followed by developed additional dataframes pertaining to online/offline orders on us holidays - success

--- main.py
+++ main.py
@@ -139,12 +139,14 @@ for year in range(2010,2021):
             try:
                 united_states_of_america_holidays_order_priority = pd.read_csv('new_csvs/united_states_of_america_holiday_orders_master.csv')
                 united_states_of_america_holidays_order_priority.loc[united_states_of_america_holidays_order_priority['Order Priority'] == order_priority]
-                united_states_of_america_holidays_order_priority.to_csv(f'new_csvs/united_states_of_america_holiday_order_priority_{order_priority}.csv')
-                # filter sales channel - online/offline
-                united_states_of_america_holidays_order_priority_online = united_states_of_america_holidays_order_priority['Sales Channel'].isin(['Online'])
-                united_states_of_america_holidays_order_priority_online.to_csv(f'new_csvs/united_states_of_america_holiday_order_priority_{order_priority}_online.csv')
-                united_states_of_america_holidays_order_priority_offline = united_states_of_america_holidays_order_priority['Sales Channel'].isin(['Offline'])
-                united_states_of_america_holidays_order_priority_offline.to_csv(f'new_csvs/united_states_of_america_holiday_order_priority_{order_priority}_offline.csv')
+                united_states_of_america_holidays_order_priority.to_csv(f'new_csvs/united_states_of_america_holiday_order_priority_{order_priority}.csv',index=False)
+                # develop dataframes per sales channel - online/offline
+                united_states_of_america_holidays_order_priority_online = united_states_of_america_holidays_order_priority.loc[(united_states_of_america_holidays_order_priority['Sales Channel'] == 'Online') & 
+                                                                                                                               (united_states_of_america_holidays_order_priority['Order Priority'] == order_priority)]
+                united_states_of_america_holidays_order_priority_online.to_csv(f'new_csvs/united_states_of_america_holiday_order_priority_{order_priority}_online.csv',index=False)
+                united_states_of_america_holidays_order_priority_offline = united_states_of_america_holidays_order_priority.loc[(united_states_of_america_holidays_order_priority['Sales Channel'] == 'Offline') & 
+                                                                                                                                (united_states_of_america_holidays_order_priority['Order Priority'] == order_priority)]
+                united_states_of_america_holidays_order_priority_offline.to_csv(f'new_csvs/united_states_of_america_holiday_order_priority_{order_priority}_offline.csv',index=False)
             except Exception as e:
                 print(f'error - cannot filter per priority - {type(e)}')
         # annual orders by region
